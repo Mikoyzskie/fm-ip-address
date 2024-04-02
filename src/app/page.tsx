bae649f9d0af166c4df09cbca31f51010a51e616
Mikoyzskie <rapidstrike13@gmail.com>
2024-04-02 18:49:25 +0800

    ip details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,13 +14,47 @@ export default function Home() {
         />
         <div className="absolute inset-0 flex flex-col justify-center items-center pt-[33px] tracking-[-0.29px]">
           <h1 className="font-medium text-[32px] text-white leading-[30px] mb-8">IP Address Tracker</h1>
-          <div className="max-w-[555px] w-full h-[58px] rounded-[15px] overflow-hidden flex">
-            <input type="text" name="" id="" placeholder="Search for any IP address or domain" className="outline-none w-full px-6 py-[18px] text-lg leading-[21px]" />
-            <button className="size-[58px] h-full bg-black flex items-center justify-center">
-              <svg xmlns="http://www.w3.org/2000/svg" width="11" height="14"><path fill="none" stroke="#FFF" stroke-width="3" d="M2 1l6 6-6 6" /></svg>
+          <div className="max-w-[555px] w-full flex ">
+            <div className="px-6 py-[18px] bg-white w-full rounded-l-[15px]">
+              <input type="text" placeholder="Search for any IP address or domain" className="outline-none w-full text-lg h-full leading-[21px]" />
+            </div>
+
+            <button className="w-[58px] h-full bg-black flex items-center justify-center rounded-r-[15px]">
+              <svg xmlns="http://www.w3.org/2000/svg" width="11" height="14"><path fill="none" stroke="#FFF" strokeWidth="3" d="M2 1l6 6-6 6" /></svg>
             </button>
           </div>
-          <div className="max-w-[1110px] relative -bottom-[81px] w-full h-[161px] bg-white shadow-md rounded-[15px]">
+          <div className="max-w-[1110px] relative -bottom-1/4 w-full bg-white shadow-md rounded-[15px] px-8 py-9 grid grid-cols-4 gap-8 items-start">
+            <div className="flex justify-between items-start">
+              <div className="flex flex-col items-start gap-[13px]">
+                <p className="tracking-[1.75px] text-xs font-bold text-[rgba(44,44,44,0.5)]">IP Address</p>
+                <h2 className="text-[#2C2C2C] text-[26px] leading-[30px] font-bold -tracking-[0.23px]">192.212.174.101</h2>
+              </div>
+              <svg width="1" height="75" viewBox="0 0 1 75" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect opacity="0.15" width="1" height="75" fill="black" />
+              </svg>
+            </div>
+            <div className="flex justify-between items-center">
+              <div className="flex flex-col items-start gap-[13px]">
+                <p className="tracking-[1.75px] text-xs font-bold text-[rgba(44,44,44,0.5)]">Location</p>
+                <h2 className="text-[#2C2C2C] text-[26px] leading-[30px] font-bold -tracking-[0.23px]">Brooklyn, NY 10001</h2>
+              </div>
+              <svg width="1" height="75" viewBox="0 0 1 75" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect opacity="0.15" width="1" height="75" fill="black" />
+              </svg>
+            </div>
+            <div className="flex justify-between items-center">
+              <div className="flex flex-col items-start gap-[13px]">
+                <p className="tracking-[1.75px] text-xs font-bold text-[rgba(44,44,44,0.5)]">Timezone</p>
+                <h2 className="text-[#2C2C2C] text-[26px] leading-[30px] font-bold -tracking-[0.23px]">UTC -05:00</h2>
+              </div>
+              <svg width="1" height="75" viewBox="0 0 1 75" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect opacity="0.15" width="1" height="75" fill="black" />
+              </svg>
+            </div>
+            <div className="flex flex-col items-start">
+              <p className="tracking-[1.75px] text-xs font-bold text-[rgba(44,44,44,0.5)]">ISP</p>
+              <h2 className="text-[#2C2C2C] text-[26px] font-bold -tracking-[0.23px]">SpaceX Starlink</h2>
+            </div>
 
           </div>
         </div>
