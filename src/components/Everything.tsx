73f65f5b1f5d8cc19472413e5300e088118e2322
Mikoyzskie <rapidstrike13@gmail.com>
2024-04-06 20:54:48 +0800

    mobile view

--- src/components/Everything.tsx
+++ src/components/Everything.tsx
@@ -140,40 +140,42 @@ export default function Everything() {
                         priority
                         className="h-[280px] w-full object-cover"
                     />
-                    <div className="absolute inset-0 flex flex-col justify-center items-center top-[33px] pt-[33px] tracking-[-0.29px]">
-                        <h1 className="font-medium text-[32px] text-white leading-[30px] mb-8">IP Address Tracker</h1>
-                        <form action={formAction} className="max-w-[555px] w-full flex ">
+                    <div className="absolute inset-0 flex flex-col pt-[33px] items-center tracking-[-0.29px]">
+                        <h1 className="font-medium text-[26px] md:text-[32px] text-white leading-[30px] mb-8">IP Address Tracker</h1>
+                        <form action={formAction} className="max-w-[555px] w-full flex px-6 md:px-0">
                             <div className="px-6 py-[18px] bg-white w-full rounded-l-[15px]">
                                 <input type="text" id="ipAddress" name="ipAddress" placeholder="Search for any IP address or domain" className="outline-none w-full text-lg h-full leading-[21px]" />
                             </div>
 
                             <SubmitButton />
                         </form>
-                        <div className="max-w-[1110px] relative -bottom-1/4 w-full bg-white shadow-md rounded-[15px] px-8 py-9 flex gap-8 justify-center z-10">
-                            <div className="flex flex-col items-start gap-[13px] basis-full">
-                                <p className="tracking-[1.75px] text-xs font-bold text-[rgba(44,44,44,0.5)]">IP Address</p>
-                                <h2 className="text-[#2C2C2C] text-[26px] leading-[30px] font-bold -tracking-[0.23px] ">{details ? details.ip : "0.0.0.0"}</h2>
+                    </div>
+                    <div className="md:px-0 px-6 absolute -bottom-full md:-bottom-1/4 z-10  max-w-[376px] md:max-w-[1110px] w-full mx-auto inset-x-0">
+                        <div className=" bg-white shadow-md rounded-[15px] px-8 py-9 flex flex-col md:flex-row gap-6 md:gap-8 justify-center  text-center md:text-start">
+                            <div className="flex flex-col md:items-start gap-[7px] md:gap-[13px] basis-full items-center">
+                                <p className="tracking-[1.75px] text-[10px] md:text-xs font-bold text-[rgba(44,44,44,0.5)]">IP Address</p>
+                                <h2 className="text-[#2C2C2C] text-[20px] md:text-[26px] leading-[30px] font-bold -tracking-[0.23px] ">{details ? details.ip : "0.0.0.0"}</h2>
                             </div>
-                            <svg className="self-center basis-1" width="1" height="75" viewBox="0 0 1 75" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg className="self-center basis-1 md:block hidden" width="1" height="75" viewBox="0 0 1 75" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <rect opacity="0.15" width="1" height="75" fill="black" />
                             </svg>
-                            <div className="flex flex-col items-start gap-[13px] basis-full">
-                                <p className="tracking-[1.75px] text-xs font-bold text-[rgba(44,44,44,0.5)]">Location</p>
-                                <h2 className="text-[#2C2C2C] text-[26px] leading-[30px] font-bold -tracking-[0.23px] ">{details ? `${details.location.city}, ${details.location.region} ${details.location.postalCode}` : "Location"}</h2>
+                            <div className="flex flex-col md:items-start gap-[7px] md:gap-[13px] basis-full items-center">
+                                <p className="tracking-[1.75px] text-[10px] md:text-xs font-bold text-[rgba(44,44,44,0.5)]">Location</p>
+                                <h2 className="text-[#2C2C2C] text-[20px] md:text-[26px] leading-[30px] font-bold -tracking-[0.23px] ">{details ? `${details.location.city}, ${details.location.region} ${details.location.postalCode}` : "Location"}</h2>
                             </div>
-                            <svg className="self-center basis-1" width="1" height="75" viewBox="0 0 1 75" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg className="self-center basis-1 md:block hidden" width="1" height="75" viewBox="0 0 1 75" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <rect opacity="0.15" width="1" height="75" fill="black" />
                             </svg>
-                            <div className="flex flex-col items-start gap-[13px] basis-full">
-                                <p className="tracking-[1.75px] text-xs font-bold text-[rgba(44,44,44,0.5)]">Timezone</p>
-                                <h2 className="text-[#2C2C2C] text-[26px] leading-[30px] font-bold -tracking-[0.23px] ">{details ? `UTC ${details.location.timezone}` : "UTC 0:00"}</h2>
+                            <div className="flex flex-col md:items-start gap-[7px] md:gap-[13px] basis-full items-center">
+                                <p className="tracking-[1.75px] text-[10px] md:text-xs font-bold text-[rgba(44,44,44,0.5)]">Timezone</p>
+                                <h2 className="text-[#2C2C2C] text-[20px] md:text-[26px] leading-[30px] font-bold -tracking-[0.23px] ">{details ? `UTC ${details.location.timezone}` : "UTC 0:00"}</h2>
                             </div>
-                            <svg className="self-center basis-1" width="1" height="75" viewBox="0 0 1 75" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg className="self-center basis-1 md:block hidden" width="1" height="75" viewBox="0 0 1 75" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <rect opacity="0.15" width="1" height="75" fill="black" />
                             </svg>
-                            <div className="flex flex-col items-start gap-[13px] basis-full">
-                                <p className="tracking-[1.75px] text-xs font-bold text-[rgba(44,44,44,0.5)]">ISP</p>
-                                <h2 className="text-[#2C2C2C] text-[26px] leading-[30px] font-bold -tracking-[0.23px] ">{details ? details.isp : "ISP"}</h2>
+                            <div className="flex flex-col md:items-start gap-[7px] md:gap-[13px] basis-full items-center">
+                                <p className="tracking-[1.75px] text-[10px] md:text-xs font-bold text-[rgba(44,44,44,0.5)]">ISP</p>
+                                <h2 className="text-[#2C2C2C] text-[20px] md:text-[26px] leading-[30px] font-bold -tracking-[0.23px] ">{details ? details.isp : "ISP"}</h2>
                             </div>
                         </div>
                     </div>
